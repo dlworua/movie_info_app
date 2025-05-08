0a7f7594ce8b3d2e5bc7d8c1e22832c164fc621e
dlworua <leegaegyum@gmail.com>
2025-05-08 21:33:35 +0900

    feat: hero 애니메이션 효과뼈대만 작업

--- lib/ui/pages/detail/detail_page.dart
+++ lib/ui/pages/detail/detail_page.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Hero(
+            tag: 'sample_image',
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Image.network(
+                'https://picsum.photos/200/300',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:movie_info_app/ui/pages/detail/detail_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -18,14 +19,28 @@ class HomePage extends StatelessWidget {
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      'https://picsum.photos/200/300',
-                      width: double.infinity,
-                      fit: BoxFit.cover,
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return DetailPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Hero(
+                      tag: 'sample_image',
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          'https://picsum.photos/200/300',
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
                 ),
