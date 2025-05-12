9eb13c013a5d18ab9d667d72b2545b92bce6da3d
dlworua <leegaegyum@gmail.com>
2025-05-12 20:05:53 +0900

    feat: 디테일 페이지 이미지,영화이름, 개봉일 ,태그라인 ,러닝타임 ,밑줄 구현

--- lib/ui/pages/detail/detail_page.dart
+++ lib/ui/pages/detail/detail_page.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
+  final String tag;
+
+  const DetailPage({super.key, required this.tag});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Hero(
-            tag: 'sample_image',
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: Image.network(
-                'https://picsum.photos/200/300',
-                width: double.infinity,
-                fit: BoxFit.cover,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Hero(
+              tag: tag,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.network(
+                    'https://picsum.photos/200/300',
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 20, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        '영화이름',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        '개봉일',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text('태그라인', style: TextStyle(height: 1.5, fontSize: 15)),
+                  Text('러닝타임', style: TextStyle(fontSize: 15)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Divider(color: Colors.grey[100], thickness: 0.5),
+            ),
+          ],
+        ),
       ),
     );
   }
