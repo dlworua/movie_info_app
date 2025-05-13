afbf23b2cdd42bdc61315c36c14391f0dd3e8a91
dlworua <leegaegyum@gmail.com>
2025-05-13 19:53:09 +0900

    feat: detail class 속성 및 객체 생성

--- lib/data/model/details.dart
+++ lib/data/model/details.dart
@@ -71,5 +71,26 @@
 //   "vote_count": 233
 // }
 
+class Movie {
+  final String title;
+  final String releaseDate;
+  final String tagline;
+  final int runtime;
+  final String overview;
+  final String posterPath;
+  final List<String> genres;
+  final double voteAverage;
+  final Map<String, dynamic> boxOffice;
 
-class Details {}
+  Movie({
+    required this.title,
+    required this.releaseDate,
+    required this.tagline,
+    required this.runtime,
+    required this.overview,
+    required this.posterPath,
+    required this.genres,
+    required this.voteAverage,
+    required this.boxOffice,
+  });
+}
