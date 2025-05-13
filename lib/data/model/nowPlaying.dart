0febb36744e86546c2b664f9f1a76b9b3db3a182
dlworua <leegaegyum@gmail.com>
2025-05-13 17:45:00 +0900

    feat: 모델폴더에 각 라벨마다 파일 만들고 클래스 생성

---
+++ lib/data/model/nowPlaying.dart
@@ -0,0 +1,21 @@
+//  {
+//       "adult": false,
+//       "backdrop_path": "/cJvUJEEQ86LSjl4gFLkYpdCJC96.jpg",
+//       "genre_ids": [
+//         10752,
+//         28
+//       ],
+//       "id": 1241436,
+//       "original_language": "en",
+//       "original_title": "Warfare",
+//       "overview": "A platoon of Navy SEALs embarks on a dangerous mission in Ramadi, Iraq, with the chaos and brotherhood of war retold through their memories of the event.",
+//       "popularity": 538.9681,
+//       "poster_path": "/j8tqBXwH2PxBPzbtO19BTF9Ukbf.jpg",
+//       "release_date": "2025-04-09",
+//       "title": "Warfare",
+//       "video": false,
+//       "vote_average": 7.14,
+//       "vote_count": 232
+//     }
+
+class Nowplaying {}
