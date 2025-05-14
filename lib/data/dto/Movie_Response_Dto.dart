18697db7e44eb8b0e1fc183db53f726f8ddba22a
dlworua <leegaegyum@gmail.com>
2025-05-14 20:32:05 +0900

    feat: movie 객체 생성

--- lib/data/dto/Movie_Response_Dto.dart
+++ lib/data/dto/Movie_Response_Dto.dart
@@ -1,1 +1,19 @@
-// 현재 상영중, 인기순, 평점, 높은순, 개봉예정
+  // {
+    //   "adult": false,
+    //   "backdrop_path": "/cJvUJEEQ86LSjl4gFLkYpdCJC96.jpg",
+    //   "genre_ids": [
+    //     10752,
+    //     28
+    //   ],
+    //   "id": 1241436,
+    //   "original_language": "en",
+    //   "original_title": "Warfare",
+    //   "overview": "A platoon of Navy SEALs embarks on a dangerous mission in Ramadi, Iraq, with the chaos and brotherhood of war retold through their memories of the event.",
+    //   "popularity": 485.3223,
+    //   "poster_path": "/j8tqBXwH2PxBPzbtO19BTF9Ukbf.jpg",
+    //   "release_date": "2025-04-09",
+    //   "title": "Warfare",
+    //   "video": false,
+    //   "vote_average": 7.133,
+    //   "vote_count": 252
+    // }
