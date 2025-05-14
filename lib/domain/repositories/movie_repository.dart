9096a9954f0eca3d758084b056a4a135df470682
dlworua <leegaegyum@gmail.com>
2025-05-14 15:41:36 +0900

    feat: 여러 폴더 삭제

--- lib/domain/repositories/movie_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../entities/movie.dart';
-
-abstract class MovieRepository {
-  Future<Movie> getMovieDetails(int movieId);
-  Future<List<Movie>> getPopularMovies();
-  Future<List<Movie>> searchMovies(String query);
-}
--- lib/domain/repository/movie_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../model/movie.dart';
-
-abstract class MovieRepository {
-  Future<List<Movie>> getNowPlayingMovies();
-  Future<List<Movie>> getPopularMovies();
-  Future<List<Movie>> getTopRatedMovies();
-  Future<List<Movie>> getUpcomingMovies();
-  Future<Movie> getMovieDetail(int movieId);
-}
