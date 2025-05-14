2729861c5432189524f41f6285bb8cd4f9e0d1a1
dlworua <leegaegyum@gmail.com>
2025-05-14 18:53:55 +0900

    feat: 폴더정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'ui/pages/home/home_screen.dart';
+import 'package:movie_info_app/theme/dark_theme.dart';
+import 'package:movie_info_app/ui/pages/home/home_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,17 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '영화 정보',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black,
-        colorScheme: ColorScheme.dark(
-          primary: Colors.white,
-          secondary: Colors.white,
-          surface: Colors.black,
-          background: Colors.black,
-        ),
-      ),
-      home: const HomeScreen(),
+      themeMode: ThemeMode.dark,
+      darkTheme: darkTheme,
+      home: HomePage(),
     );
   }
 }
